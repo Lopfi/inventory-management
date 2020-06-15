78c8b8798411c8f008ab096fff7e75530ab27fee
RobinD95 <lopfi95@gmail.com>
2020-06-15 19:33:12 +0200

    add all get and post responses

--- app.js
+++ app.js
@@ -11,13 +11,94 @@ let db = new sqlite3.Database("./db/content.db", sqlite3.OPEN_READWRITE | sqlite
   }
 });
 
+CREATE TABLE [IF NOT EXISTS] items (
+	itemid INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT,
+  locationid INTEGER NOT NULL,
+  image TEXT
+) [WITHOUT ROWID];
+CREATE TABLE [IF NOT EXISTS] locations (
+	locationid INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT,
+  image TEXT
+) [WITHOUT ROWID];
+
 
 app.use(express.static(path.join(__dirname, "public")));
-app.get("/", (req, res) => res.sendfile(./content/index.html));
-app.get("/item", (req, res) => {
-  let id = req.query.id;
+app.use(express.urlencoded());
+app.use(express.json());
 
+
+app.get("/", (req, res) => res.sendfile(./content/index.html));
+
+app.get("/item?id=[itemid]", (req, res) => {
+  let itemid = req.query.id;
+  let sql = `SELECT *
+           FROM items
+           WHERE  itemid = ?`;
+  db.get(sql, [itemid], (err, row) => {
+    if (err) {
+      console.log("Error: " + err.message);
+    } else {
+      res.send(JSON.stringify({"id": row.id,
+        "name": row.name,
+        "description": row.description,
+        "location": row.location,
+        "image": row.image}));
+    }
+  });
 });
+
+app.get("/location?id=[locationid]", (req, res) => {
+  let locationid = req.query.id;
+  let sql = `SELECT *
+           FROM locations
+           WHERE  locationid = ?`;
+  db.get(sql, [locationid], (err, row) => {
+    if (err) {
+      console.log("Error: " + err.message);
+    } else {
+      res.send(JSON.stringify({"id": row.id,
+        "name": row.name,
+        "description": row.description,
+        "image": row.image}));
+    }
+  });
+});
+
+app.post("/delitem", (req, res) => {
+  let itemid = request.body.itemid;
+  let sql = `DELETE FROM items WHERE itemid = ?`;
+  db.run(sql, [itemid]);
+});
+
+app.post("/dellocation", (req, res) => {
+  let locationid = request.body.locationid;
+  let sql = `DELETE FROM locations WHERE locationid = ?`;
+  db.run(sql, [locationid]);
+});
+
+app.post("/additem", (req, res) => {
+  let itemid =  request.body.itemid;
+  let name = request.body.name;
+  let description = request.body.description;
+  let location = request.body.location;
+  let image = request.body.image;
+  let sql = `INSERT INTO items VALUES(?,?,?,?,?)`;
+  db.run(sql, [itemid, name, description, location, image]);
+});
+
+app.post("/addlocation", (req, res) => {
+  let itemid =  request.body.itemid;
+  let name = request.body.name;
+  let description = request.body.description;
+  let image = request.body.image;
+  let sql = `INSERT INTO items VALUES(?,?,?,?)`;
+  db.run(sql, [itemid, name, description, image]);
+});
+
 app.get("*", (req, res) => res.send("404"));
 
 app.listen(80, () => {
