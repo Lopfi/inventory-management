4fa90130464a78b82dcf9ec2e68e58d08e9b56eb
Lopfi <lopfi95@gmail.com>
2022-07-02 23:20:26 +0200

    fix label generation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,147 +1,168 @@
-const express = require('express');
-const path = require('path');
-const { sendSqlQuery, executeSqlQuery, getImagePaths, generatePDF } = require('../utils');
+const express = require("express");
+const path = require("path");
+const {
+  sendSqlQuery,
+  executeSqlQuery,
+  getImagePaths,
+  generatePDF,
+} = require("../utils");
 
 const router = express.Router();
 
 module.exports = function (db, upload) {
-    router
-        .route('/items')
-        .get((req, res) => {
-            const { limit, offset } = req.query;
-            let sql = `
+  router
+    .route("/items")
+    .get((req, res) => {
+      const { limit, offset } = req.query;
+      let sql = `
             SELECT items.id as itemID, items.name as itemName, items.image, locations.id as locationID, locations.name as locationName
             FROM items
                      LEFT JOIN locations
                                ON items.location = locations.id
             LIMIT ? OFFSET ?`;
-            sendSqlQuery(db, sql, [parseInt(limit), parseInt(offset)], res);
-        })
-        .put(upload.array('files'), (req, res) => {
-            const { name, description, amount, location } = req.body;
-            let sql = `INSERT INTO items (name, description, amount, location, image)
+      sendSqlQuery(db, sql, [parseInt(limit), parseInt(offset)], res);
+    })
+    .put(upload.array("files"), (req, res) => {
+      const { name, description, amount, location } = req.body;
+      let sql = `INSERT INTO items (name, description, amount, location, image)
                    VALUES (?, ?, ?, ?, ?)`;
-            executeSqlQuery(
-                db,
-                sql,
-                [name, description, amount, location, getImagePaths(req.files)],
-                res,
-                'successfully added item to database'
-            );
-        });
+      executeSqlQuery(
+        db,
+        sql,
+        [name, description, amount, location, getImagePaths(req.files)],
+        res,
+        "successfully added item to database"
+      );
+    });
 
-    router
-        .route('/locations')
-        .get((req, res) => {
-            const { limit, offset } = req.query;
-            let sql = `SELECT locations.id as locationID, locations.name as locationName, locations.image, count(items.id) AS amount
+  router
+    .route("/locations")
+    .get((req, res) => {
+      const { limit, offset } = req.query;
+      let sql = `SELECT locations.id as locationID, locations.name as locationName, locations.image, count(items.id) AS amount
                    FROM locations
                             LEFT JOIN items
                                       ON locations.id = items.location
                    GROUP BY locations.id
                    LIMIT ? OFFSET ?`;
-            sendSqlQuery(db, sql, [parseInt(limit), parseInt(offset)], res);
-        })
-        .put(upload.array('files'), (req, res) => {
-            const { name, description } = req.body;
-            let sql = `INSERT INTO locations (name, description, image)
+      sendSqlQuery(db, sql, [parseInt(limit), parseInt(offset)], res);
+    })
+    .put(upload.array("files"), (req, res) => {
+      const { name, description } = req.body;
+      let sql = `INSERT INTO locations (name, description, image)
                    VALUES (?, ?, ?)`;
-            executeSqlQuery(
-                db,
-                sql,
-                [name, description, getImagePaths(req.files)],
-                res,
-                'successfully added location to database'
-            );
-        });
-
-    router.get('/locations/labels', (req, res) => {
-        const { limit, offset } = req.query;
-        console.log('labels');
-        let sql = `SELECT id as locationID, name as locationName FROM locations LIMIT ? OFFSET ?`;
-        db.query(sql, [parseInt(limit), parseInt(offset)], (err, rows) => {
-            if (err) {
-                console.log('Error: ' + err.message);
-                res.status(500).json({ message: 'database error' });
-            } else {
-                //TODO: add fail check
-                generatePDF(rows).then(() => {
-                    res.status(200).download(path.join(__dirname, '../utils/temp/labels.pdf'));
-                });
-            }
-        });
+      executeSqlQuery(
+        db,
+        sql,
+        [name, description, getImagePaths(req.files)],
+        res,
+        "successfully added location to database"
+      );
     });
-    router
-        .route('/locations/:id')
-        .get((req, res) => {
-            let sql = `SELECT id as locationID, name as locationName, image, description
+
+  router.get("/locations/labels", (req, res) => {
+    console.log("generating labels");
+    let sql = `SELECT id as locationID, name as locationName FROM locations LIMIT ?`;
+    db.query(sql, [100], (err, rows) => {
+      if (err) {
+        console.log("Error: " + err.message);
+        res.status(500).json({ message: "database error" });
+      } else {
+        //TODO: add fail check
+        generatePDF(rows);
+        setTimeout(() => {}, 500);
+        res
+          .status(200)
+          .download(path.join(__dirname, "../utils/temp/labels.pdf"));
+      }
+    });
+  });
+
+  router
+    .route("/locations/:id")
+    .get((req, res) => {
+      let sql = `SELECT id as locationID, name as locationName, image, description
                    FROM locations
                    WHERE id = ?`;
-            sendSqlQuery(db, sql, [req.params.id], res);
-        })
-        .patch((req, res) => {
-            const { id, name, description, amount, location } = req.body;
-            let sql = `UPDATE items SET name=?, description=?, amount=?, location=? 
+      sendSqlQuery(db, sql, [req.params.id], res);
+    })
+    .patch((req, res) => {
+      const { id, name, description, amount, location } = req.body;
+      let sql = `UPDATE items SET name=?, description=?, amount=?, location=? 
             WHERE id=?`;
-            executeSqlQuery(
-                db,
-                sql,
-                [name, description, amount, location, id],
-                res,
-                'successfully updated item in database'
-            );
-        })
-        .delete((req, res) => {
-            let sql = `DELETE
+      executeSqlQuery(
+        db,
+        sql,
+        [name, description, amount, location, id],
+        res,
+        "successfully updated item in database"
+      );
+    })
+    .delete((req, res) => {
+      let sql = `DELETE
                     FROM locations
                     WHERE id = ?`;
-            executeSqlQuery(db, sql, [req.params.id], res, 'successfully deleted from locations');
-        });
-
-    router.get('/locations/:id/items', (req, res) => {
-        let sql = `SELECT id as itemID, name as itemName, image, description, location, amount
-                   FROM items
-                   WHERE id = ?`;
-        sendSqlQuery(db, sql, [req.params.id], res);
+      executeSqlQuery(
+        db,
+        sql,
+        [req.params.id],
+        res,
+        "successfully deleted from locations"
+      );
     });
 
-    router
-        .route('/items/:id')
-        .get((req, res) => {
-            let sql = `SELECT id as itemID, name as itemName, image, description, location as locationID, amount
+  router.get("/locations/:id/items", (req, res) => {
+    let sql = `SELECT id as itemID, name as itemName, image, description, location, amount
                    FROM items
                    WHERE id = ?`;
-            sendSqlQuery(db, sql, [req.params.id], res);
-        })
-        .delete((req, res) => {
-            let sql = `DELETE
-                   FROM items
-                   WHERE id = ?`;
-            executeSqlQuery(db, sql, [req.params.id], res, 'successfully deleted from items');
-        });
+    sendSqlQuery(db, sql, [req.params.id], res);
+  });
 
-    router.get('/search', (req, res) => {
-        var { string, limit, offset } = req.query;
-        string = '%' + string + '%';
-        let sql = `SELECT locations.id as locationID, locations.name as locationName, locations.image, count(items.id) AS amount
+  router
+    .route("/items/:id")
+    .get((req, res) => {
+      let sql = `SELECT id as itemID, name as itemName, image, description, location as locationID, amount
+                   FROM items
+                   WHERE id = ?`;
+      sendSqlQuery(db, sql, [req.params.id], res);
+    })
+    .delete((req, res) => {
+      let sql = `DELETE
+                   FROM items
+                   WHERE id = ?`;
+      executeSqlQuery(
+        db,
+        sql,
+        [req.params.id],
+        res,
+        "successfully deleted from items"
+      );
+    });
+
+  router.get("/search", (req, res) => {
+    var { string, limit, offset } = req.query;
+    string = "%" + string + "%";
+    let sql = `SELECT locations.id as locationID, locations.name as locationName, locations.image, count(items.id) AS amount
                    FROM locations
                             LEFT JOIN items
                                       ON locations.id = items.location
                    WHERE items.name LIKE ? OR locations.name LIKE ? OR items.description LIKE ? OR locations.description LIKE ?
                    LIMIT ? OFFSET ?`;
-        sendSqlQuery(
-            db,
-            sql,
-            [string, string, string, string, parseInt(limit), parseInt(offset)],
-            res
-        );
-    });
-
-    router.get('/img/:img', (req, res) =>
-        res.status(200).sendFile(path.join(__dirname, '../../db/images/', req.params.img))
+    sendSqlQuery(
+      db,
+      sql,
+      [string, string, string, string, parseInt(limit), parseInt(offset)],
+      res
     );
+  });
 
-    router.get('*', (req, res) => res.status(404).send('404'));
+  router.get("/img/:img", (req, res) =>
+    res
+      .status(200)
+      .sendFile(path.join(__dirname, "../../db/images/", req.params.img))
+  );
 
-    return router;
+  router.get("*", (req, res) => res.status(404).send("404"));
+
+  return router;
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,62 +1,76 @@
-const path = require('path');
+const path = require("path");
 
 exports.sendSqlQuery = function (db, sql, params, res) {
-    db.query(sql, params, (err, rows) => {
-        if (err) {
-            console.log('Error: ' + err.message);
-            res.status(500).json({ message: 'database error' });
-        } else {
-            let response = [];
-            rows.forEach((row) => {
-                response.push(row);
-            });
-            res.status(200).json(response);
-        }
-    });
+  db.query(sql, params, (err, rows) => {
+    if (err) {
+      console.log("Error: " + err.message);
+      res.status(500).json({ message: "database error" });
+    } else {
+      let response = [];
+      rows.forEach((row) => {
+        response.push(row);
+      });
+      res.status(200).json(response);
+    }
+  });
 };
 
 exports.executeSqlQuery = function (db, sql, params, res, success) {
-    db.query(sql, params, (err, rows) => {
-        if (err) {
-            console.log('Error: ' + err.message);
-            res.status(500).json({ message: 'database error' });
-        } else res.status(200).json({ message: success });
-    });
+  db.query(sql, params, (err, rows) => {
+    if (err) {
+      console.log("Error: " + err.message);
+      res.status(500).json({ message: "database error" });
+    } else res.status(200).json({ message: success });
+  });
 };
 
 exports.getImagePaths = function (images) {
-    let paths = [];
-    if (images.length > 0) for (const image of images) paths.push(image.filename);
-    else paths.push('Default.png');
-    return paths;
+  let paths = [];
+  if (images.length > 0) for (const image of images) paths.push(image.filename);
+  else paths.push("Default.png");
+  return paths;
 };
 
-function generateQR(input, filename) {
-    require('fs').writeFileSync(
-        path.join(__dirname, filename + '.png'),
-        require('qr-image').imageSync(input)
-    );
+async function generateQR(input, filename) {
+  require("fs").writeFileSync(
+    path.join(__dirname, filename + ".png"),
+    require("qr-image").imageSync(input)
+  );
 }
 
 exports.generatePDF = async function (rows) {
-    const PDFDocument = require('pdfkit');
-    const fs = require('fs');
+  const PDFDocument = require("pdfkit");
+  const fs = require("fs");
 
-    const doc = new PDFDocument();
-    doc.pipe(fs.createWriteStream(path.join(__dirname, '/temp/labels.pdf')));
+  const doc = new PDFDocument();
+  doc.pipe(fs.createWriteStream(path.join(__dirname, "/temp/labels.pdf")));
+  let pages = 0;
+  for (let i = 0; i < rows.length; i++) {
+    const location = rows[i];
+    let posX = (i % 2) * 200 + 30;
+    let posY = Math.floor(i / 2) * 100 + 30 - pages * 700;
 
-    for (let i = 0; i < rows.length; i++) {
-        const location = rows[i];
-        let posX = (i % 2) * 200 + 30;
-        let posY = Math.floor(i / 2) * 100 + 30;
-        //doc.addPage();
-        doc.fontSize(25).text(location.locationName, posX + 90, posY + 8);
-        doc.fontSize(15).text('ID: ' + location.locationID, posX + 90, posY + 30);
-        generateQR('/locations/' + location.locationID, '/temp/' + location.locationID);
-        doc.image(path.join(__dirname, '/temp/' + location.locationID + '.png'), posX, posY, {
-            width: 80,
-            height: 80,
-        });
+    doc.fontSize(25).text(location.locationName, posX + 90, posY + 8);
+    doc.fontSize(15).text("ID: " + location.locationID, posX + 90, posY + 30);
+    await generateQR(
+      "/locations/" + location.locationID,
+      "/temp/" + location.locationID
+    );
+    doc.image(
+      path.join(__dirname, "/temp/" + location.locationID + ".png"),
+      posX,
+      posY,
+      {
+        width: 80,
+        height: 80,
+      }
+    );
+
+    if (posY == 630 && posX == 230) {
+      doc.addPage();
+      pages++;
     }
-    doc.end();
+  }
+  doc.end();
+  setTimeout(() => {}, 1000);
 };
